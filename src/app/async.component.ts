a931bc69f374059033e640bac05606c7bb62be10
pawel <pawkonik@gmail.com>
2018-08-07 23:52:16 +0200

    Add fromEvent implementation

--- src/app/async.component.ts
+++ src/app/async.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {fromEvent, Observable} from 'rxjs/index';
+import {delay, filter, map} from 'rxjs/internal/operators';
 
 @Component({
-  /*selector: 'app-async',*/
-  templateUrl: './async.component.html'
+  templateUrl: './async.component.html',
+  styleUrls: ['./async.component.css']
 })
-export class AsyncComponent implements OnInit {
+export class AsyncComponent implements AfterViewInit {
+  source$: Observable<any>;
+  left: string;
+  top: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  fromEventTest(): void {
+    this.source$.subscribe(
+      value => this.onNext(value),
+      err => console.log(`error: ${err}`),
+      () => console.log('completed')
+    );
+  }
+  onNext(value: MouseEvent) {
+    this.left = value.x.toString();
+    this.top = value.y.toString();
+    console.log('x ' + this.left, ' y  ' + this.top);
   }
 
+  ngAfterViewInit() {
+    this.source$ = fromEvent(document, 'mousemove')
+      .pipe(
+        map((e: MouseEvent) => {
+          return {
+            x: e.clientX,
+            y: e.clientY
+          };
+        }),
+        filter<MouseEvent>(e => e.x < 910),
+        delay(200));
+    this.fromEventTest();
+  }
 }
